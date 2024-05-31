fbef3f21a46a6130ad89f7916166067d5d86684f
Ashish Mohite <ashishmohite1996@gmail.com>
2024-05-31 20:40:01 +0530

    Need to check audio track queue

--- shared/src/commonMain/kotlin/com/mak/pocketnotes/data/repository/AudioRepository.kt
+++ shared/src/commonMain/kotlin/com/mak/pocketnotes/data/repository/AudioRepository.kt
@@ -9,6 +9,36 @@ class AudioRepository : IAudioRepository {
     override val tracks: List<PlayableEpisode>
         get() = _tracks
 
+//    val episodes: List<PlayableEpisode>
+//        field: MutableList<PlayableEpisode> = mutableListOf()
 
+    fun addEpisode(episode: PlayableEpisode) {
+        _tracks.add(episode)
+    }
 
+    fun removeEpisode(episode: PlayableEpisode) {
+        _tracks.remove(episode)
+    }
+
+    fun clearPlaylist() {
+        _tracks.clear()
+    }
+
+    fun moveSong(from: Int, to: Int) {
+        if (from == to) return // moved to same position
+        val episode = _tracks.removeAt(from)
+        if (from < to)
+            _tracks.add(to - 1, episode)
+        else
+            _tracks.add(to, episode)
+    }
+
+    fun getEpisodeAt(index: Int): PlayableEpisode? {
+        if (index < 0 || index >= _tracks.size || _tracks.isEmpty()) return null
+        return _tracks.getOrNull(index)
+    }
+
+    fun shufflePlaylist() {
+        _tracks.shuffle()
+    }
 }
