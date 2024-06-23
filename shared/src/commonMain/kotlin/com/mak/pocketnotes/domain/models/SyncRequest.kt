dd8f52420d14f626267703916c31aefeef5df4bb
Ashish Mohite <ashishmohite1996@gmail.com>
2024-06-23 21:44:00 +0530

    Add last_sync queries

---
+++ shared/src/commonMain/kotlin/com/mak/pocketnotes/domain/models/SyncRequest.kt
@@ -0,0 +1,9 @@
+package com.mak.pocketnotes.domain.models
+
+internal enum class SyncRequest {
+    BEST_PODCASTS,
+    CURATED_PODCASTS,
+    PODCAST_DETAILS,
+    PODCAST_RECOMMENDATIONS,
+    SEARCH
+}
