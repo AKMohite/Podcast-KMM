ffa5e09d7e2081c2db27f0a57d751942ef51ab81
Ashish Mohite <ashishmohite1996@gmail.com>
2024-06-24 21:37:19 +0530

    Handle dispatchers for best podcasts

--- shared/src/commonMain/kotlin/com/mak/pocketnotes/domain/usecase/GetBestPodcasts.kt
+++ shared/src/commonMain/kotlin/com/mak/pocketnotes/domain/usecase/GetBestPodcasts.kt
@@ -1,9 +1,11 @@
 package com.mak.pocketnotes.domain.usecase
 
+import com.mak.pocketnotes.data.util.Dispatcher
 import com.mak.pocketnotes.domain.mapper.PodcastMapper
 import com.mak.pocketnotes.domain.models.Podcast
 import com.mak.pocketnotes.local.database.dao.ITrendingPodcastDAO
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -11,9 +13,12 @@ import org.koin.core.component.inject
 class GetBestPodcasts: KoinComponent {
     private val dao: ITrendingPodcastDAO by inject()
     private val mapper: PodcastMapper by inject()
+    private val dispatcher: Dispatcher by inject()
 
     operator fun invoke(): Flow<List<Podcast>> {
         return dao.getBestPodcasts()
+            .flowOn(dispatcher.io)
             .map { entities -> mapper.entityToModels(entities) }
+            .flowOn(dispatcher.computation)
     }
 }
--- shared/src/commonMain/kotlin/com/mak/pocketnotes/domain/usecase/RefreshBestPodcasts.kt
+++ shared/src/commonMain/kotlin/com/mak/pocketnotes/domain/usecase/RefreshBestPodcasts.kt
@@ -1,18 +1,22 @@
 package com.mak.pocketnotes.domain.usecase
 
 import com.mak.pocketnotes.data.remote.IPocketNotesAPI
+import com.mak.pocketnotes.data.util.Dispatcher
 import com.mak.pocketnotes.domain.mapper.PocketMapper
 import com.mak.pocketnotes.domain.models.Podcast
 import com.mak.pocketnotes.local.database.DatabaseTransactionRunner
 import com.mak.pocketnotes.local.database.dao.IPodcastDAO
 import com.mak.pocketnotes.local.database.dao.ITrendingPodcastDAO
+import com.mak.pocketnotes.local.database.dao.PodcastEntity
 import com.mak.pocketnotes.local.database.dao.TrendingPodcastEntity
+import kotlinx.coroutines.withContext
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 class RefreshBestPodcasts: KoinComponent {
 
     private val api: IPocketNotesAPI by inject()
+    private val dispatcher: Dispatcher by inject()
     private val transactionRunner: DatabaseTransactionRunner by inject()
     private val podcastDAO: IPodcastDAO by inject()
     private val trendingPodcastDAO: ITrendingPodcastDAO by inject()
@@ -35,12 +39,20 @@ class RefreshBestPodcasts: KoinComponent {
                 page = page
             )
         }
+        updateLocal(podcasts, page, trendingPodcasts)
+//        TODO: this is for iOS we can remove it after local db for iOS is implemented
+        return mapper.podcast.entityToModels(podcasts)
+    }
+
+    private suspend fun updateLocal(
+        podcasts: List<PodcastEntity>,
+        page: Int,
+        trendingPodcasts: List<TrendingPodcastEntity>
+    ) = withContext(dispatcher.io) {
         transactionRunner {
             podcastDAO.insertPodcasts(podcasts)
             trendingPodcastDAO.deletePage(page)
             trendingPodcastDAO.upsertPage(trendingPodcasts)
         }
-//        TODO: this is for iOS we can remove it after local db for iOS is implemented
-        return mapper.podcast.entityToModels(podcasts)
     }
 }
