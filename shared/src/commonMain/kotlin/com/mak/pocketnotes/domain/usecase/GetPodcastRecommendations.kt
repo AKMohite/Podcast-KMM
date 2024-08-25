067c85928dfc38b637c4b940fac394ed3bc51c64
Ashish Mohite <ashishmohite1996@gmail.com>
2024-08-25 21:00:56 +0530

    Get related podcasts on io

--- shared/src/commonMain/kotlin/com/mak/pocketnotes/domain/usecase/GetPodcastRecommendations.kt
+++ shared/src/commonMain/kotlin/com/mak/pocketnotes/domain/usecase/GetPodcastRecommendations.kt
@@ -1,9 +1,11 @@
 package com.mak.pocketnotes.domain.usecase
 
+import com.mak.pocketnotes.data.util.Dispatcher
 import com.mak.pocketnotes.domain.models.Podcast
 import com.mak.pocketnotes.domain.store.RelatedPodcastsStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -13,6 +15,7 @@ import org.mobilenativefoundation.store.store5.StoreReadResponse
 class GetPodcastRecommendations: KoinComponent {
 
     private val store: RelatedPodcastsStore by inject()
+    private val dispatcher: Dispatcher by inject()
 
     operator fun invoke(id: String): Flow<List<Podcast>> {
         return store.invoke(id).stream(StoreReadRequest.cached(key = id, refresh = false))
@@ -20,6 +23,6 @@ class GetPodcastRecommendations: KoinComponent {
                 storeResponse is StoreReadResponse.Data
             }.map { storeResponse ->
                 storeResponse.requireData()
-            }
+            }.flowOn(dispatcher.io)
     }
 }
