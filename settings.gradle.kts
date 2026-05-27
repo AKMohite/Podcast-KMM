d1ae6c5cd1675fce5dd6720dbabba2ba5dc23545
Ashish Mohite <ashishmohite1996@gmail.com>
2026-05-27 15:36:28 +0530

    Update libraries

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,17 +4,6 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
     }
-    // latest version of r8
-    buildscript {
-        repositories {
-            maven {
-                url = uri("https://storage.googleapis.com/r8-releases/raw/main")
-            }
-            dependencies {
-                classpath("com.android.tools:r8:f4c9082149ea97a824cc29130e51df1cb32a1df1")
-            }
-        }
-    }
 }
 
 dependencyResolutionManagement {
