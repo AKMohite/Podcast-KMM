035de509d97a56e4bf079bf7ceec18a3f2b212d1
Ashish Mohite <ashishmohite1996@gmail.com>
2026-05-31 12:59:28 +0530

    Move DiscoverMediumHeader to separate file

---
+++ androidApp/src/main/java/com/mak/pocketnotes/android/feature/discover/components/DiscoverMediumHeader.kt
@@ -0,0 +1,14 @@
+package com.mak.pocketnotes.android.feature.discover.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.mak.pocketnotes.domain.models.Podcast
+
+@Composable
+fun DiscoverMediumHeader(
+    podcasts: List<Podcast>,
+    modifier: Modifier,
+    onPodcastClick: (String) -> Unit
+) {
+
+}
