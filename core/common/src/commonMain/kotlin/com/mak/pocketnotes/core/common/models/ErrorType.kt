d34c9d69b0bbd78d5dce6ba84f913bb075b7706c
Ashish Mohite <ashishmohite1996@gmail.com>
2026-07-14 20:14:56 +0530

    Add ErrorType.kt and SafeAPICall.kt

---
+++ core/common/src/commonMain/kotlin/com/mak/pocketnotes/core/common/models/ErrorType.kt
@@ -0,0 +1,10 @@
+package com.mak.pocketnotes.core.common.models
+
+enum class ErrorType {
+    PARSE,
+    NO_CONNECTIVITY,
+    NOT_FOUND,
+    UNAUTHORIZED,
+    SERVER_ERROR,
+    UNKNOWN
+}
---
+++ core/remote/src/commonMain/kotlin/com/mak/pocketnotes/core/remote/utils/SafeAPICall.kt
@@ -0,0 +1,50 @@
+package com.mak.pocketnotes.core.remote.utils
+
+import com.mak.pocketnotes.core.common.models.ErrorType
+import io.ktor.client.call.body
+import io.ktor.client.plugins.HttpRequestTimeoutException
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.isSuccess
+import io.ktor.serialization.JsonConvertException
+import kotlinx.coroutines.CancellationException
+import kotlinx.io.IOException
+import kotlinx.serialization.SerializationException
+
+// Define Result and Error types if not already present
+sealed interface RemoteResult<out T> {
+    data class Success<out T>(val data: T) : RemoteResult<T>
+    data class Failure(val error: ErrorType) : RemoteResult<Nothing>
+}
+
+internal suspend inline fun <reified T> safeApiCall(
+    block: suspend () -> HttpResponse
+): RemoteResult<T> {
+    return try {
+        val response = block()
+        if (response.status.isSuccess()) {
+            RemoteResult.Success(response.body<T>())
+        } else {
+            RemoteResult.Failure(response.status.toErrorType())
+        }
+    } catch (e: Exception) {
+        when (e) {
+            is JsonConvertException, is SerializationException -> RemoteResult.Failure(ErrorType.PARSE)
+            is HttpRequestTimeoutException, is IOException -> RemoteResult.Failure(ErrorType.NO_CONNECTIVITY)
+            is CancellationException -> throw e
+            else -> RemoteResult.Failure(ErrorType.UNKNOWN)
+        }
+    }
+}
+
+private fun HttpStatusCode.toErrorType(): ErrorType = when (value) {
+    in HttpStatusCode.InternalServerError.value..HttpStatusCode(
+        599,
+        "Network Connect Timeout"
+    ).value -> ErrorType.SERVER_ERROR
+
+    HttpStatusCode.NotFound.value -> ErrorType.NOT_FOUND
+    HttpStatusCode.Unauthorized.value -> ErrorType.UNAUTHORIZED
+    HttpStatusCode.RequestTimeout.value -> ErrorType.NO_CONNECTIVITY
+    else -> ErrorType.UNKNOWN
+}
