b3e27e0cf0e592243489a30e6c2fb47ccb2ed9b4
Ashish Mohite <ashishmohite1996@gmail.com>
2026-08-06 11:50:46 +0530

    Add Intializer to load koin

--- androidApp/src/main/java/com/mak/pocketnotes/android/PocketCastApp.kt
+++ androidApp/src/main/java/com/mak/pocketnotes/android/PocketCastApp.kt
@@ -1,20 +1,5 @@
 package com.mak.pocketnotes.android
 
 import android.app.Application
-import com.mak.pocketnotes.android.di.appModule
-import com.mak.pocketnotes.di.getSharedModules
-import com.mak.pocketnotes.di.mediaModule
-import com.mak.pocketnotes.di.mediaModuleV2
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
 
-internal class PocketCastApp: Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-        startKoin {
-            androidContext(this@PocketCastApp)
-            modules(appModule + getSharedModules() + mediaModuleV2 + mediaModule)
-        }
-    }
-}
+internal class PocketCastApp : Application()
---
+++ androidApp/src/main/java/com/mak/pocketnotes/android/initializer/KoinInitializer.kt
@@ -0,0 +1,22 @@
+package com.mak.pocketnotes.android.initializer
+
+import android.content.Context
+import androidx.startup.Initializer
+import com.mak.pocketnotes.android.di.appModule
+import com.mak.pocketnotes.di.getSharedModules
+import com.mak.pocketnotes.di.mediaModule
+import com.mak.pocketnotes.di.mediaModuleV2
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.KoinApplication
+import org.koin.core.context.startKoin
+
+class KoinInitializer : Initializer<KoinApplication> {
+    override fun create(context: Context): KoinApplication {
+        return startKoin {
+            androidContext(context)
+            modules(appModule + getSharedModules() + mediaModuleV2 + mediaModule)
+        }
+    }
+
+    override fun dependencies(): List<Class<out Initializer<*>?>?> = emptyList()
+}
