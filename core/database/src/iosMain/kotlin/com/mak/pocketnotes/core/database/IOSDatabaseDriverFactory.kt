ef0709496912a4e329e1e494af90c4dc70807a53
Ashish Mohite <ashishmohite1996@gmail.com>
2026-08-21 12:10:05 +0530

    Resolve warnings

--- core/database/src/iosMain/kotlin/com/mak/pocketnotes/core/database/IOSDatabaseDriverFactory.kt
+++ core/database/src/iosMain/kotlin/com/mak/pocketnotes/core/database/IOSDatabaseDriverFactory.kt
@@ -2,8 +2,8 @@ package com.mak.pocketnotes.core.database
 
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
-import com.mak.pocketnotes.PocketDatabase
 import com.mak.pocketnotes.core.database.DatabaseDriverFactory.Companion.LOCAL_DB
+import com.mak.pocketnotes.core.database.queries.PocketDatabase
 
 internal class IOSDatabaseDriverFactory : DatabaseDriverFactory {
     override fun createDriver(): SqlDriver {
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidKMMLibrary)
-    alias(libs.plugins.sqldelight)
     alias(libs.plugins.skie)
     alias(libs.plugins.kotlinxSerialization)
 }
@@ -29,7 +28,7 @@ kotlin {
     }
 
     sourceSets {
-        val commonMain by getting {
+        commonMain {
             compilerOptions {
                 freeCompilerArgs.add("-opt-in=kotlin.time.ExperimentalTime")
             }
@@ -59,12 +58,12 @@ kotlin {
                 implementation(libs.kotlinx.datetime)
             }
         }
-        val commonTest by getting {
+        commonTest {
             dependencies {
                 implementation(kotlin("test"))
             }
         }
-        val androidMain by getting {
+        androidMain {
             dependencies {
                 implementation(libs.ktor.client.android)
                 implementation(libs.koin.android)
@@ -80,35 +79,16 @@ kotlin {
                 implementation(libs.androidx.datastore.tink)
             }
         }
-//        val androidUnitTest by getting
-//        val androidAndroidTest by getting
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
+
+        iosMain {
             dependencies {
             }
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
         }
-        val iosX64Test by getting
-        val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
-        val iosTest by creating {
-            dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
-        }
-    }
-}
 
-sqldelight {
-    databases {
-        create("PocketDatabase") {
-            packageName.set("com.mak.pocketnotes")
+        iosTest {
+            dependencies {
+                implementation(kotlin("test"))
+            }
         }
     }
 }
